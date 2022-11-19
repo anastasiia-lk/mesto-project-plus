f262ec7c8dfa9a3493febcd1bb6e90285b3d941e
Anastasiia Duplishcheva <a.duplishcheva@gmail.com>
2022-11-19 23:15:48 +0300

    add controllers and routes in cards

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -22,3 +22,25 @@ export const createUser = (req: Request, res: Response) => {
     .then((user) => res.send({ data: user }))
     .catch((err) => res.status(500).send({ message: err.message }));
 };
+
+export const updateUser = (req: any, res: Response) => {
+  const { name, about } = req.body;
+  User.findByIdAndUpdate(
+    req.user._id,
+    { name, about },
+    { new: true, runValidators: true },
+  )
+    .then((user) => res.send({ data: user }))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
+
+export const updateAvatar = (req: any, res: Response) => {
+  const { avatar } = req.body;
+  User.findByIdAndUpdate(
+    req.user._id,
+    { avatar },
+    { new: true, runValidators: true },
+  )
+    .then((setAvatar) => res.send({ data: setAvatar }))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -3,6 +3,8 @@ import {
   getUsers,
   createUser,
   getUser,
+  updateUser,
+  updateAvatar,
 } from '../controllers/users';
 
 const usersRouter = Router();
@@ -10,5 +12,7 @@ const usersRouter = Router();
 usersRouter.get('/', getUsers);
 usersRouter.post('/', createUser);
 usersRouter.get('/:id', getUser);
+usersRouter.patch('/me', updateUser);
+usersRouter.patch('/me/avatar', updateAvatar);
 
 export default usersRouter;
