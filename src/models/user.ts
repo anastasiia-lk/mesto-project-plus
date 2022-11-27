1f8963854cd651615dcd4af89f13ccfeeaadc9ca
Anastasiia Duplishcheva <a.duplishcheva@gmail.com>
2022-11-27 16:22:38 +0300

    add email&password in user schema

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,12 +1,29 @@
 import { Schema, model } from 'mongoose';
+import validator from 'validator';
 
 export interface IUser {
+  email: string,
+  password: string,
   name: string;
   about: string;
   avatar: string;
 }
 
 const userSchema = new Schema<IUser>({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: (email: string) => validator.isEmail(email),
+      message: 'Указан неверный формат почты',
+    },
+  },
+  password: {
+    type: String,
+    select: false,
+    required: true,
+  },
   name: {
     type: String,
     minLenght: 2,
