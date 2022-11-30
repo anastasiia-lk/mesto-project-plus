d6e094185b86287cb5d1c2a37f004f3034499ef0
Anastasiia Duplishcheva <a.duplishcheva@gmail.com>
2022-11-30 20:59:15 +0300

    add cards controller errors

--- src/controllers/cards.ts
+++ src/controllers/cards.ts
@@ -2,21 +2,21 @@ import { Request, Response, NextFunction } from 'express';
 import Card from '../models/card';
 import NotFound from '../utils/errors/NotFound';
 import NotAllowed from '../utils/errors/NotAllowed';
+import BadReq from '../utils/errors/BadReq';
 
 import {
   INVALID_DATA_ERROR,
-  NOT_FOUND_ERROR,
   DEFAULT_ERROR,
   CustomRequest,
 } from '../utils/constants';
 
-export const getCards = (_req: Request, res: Response) => {
+export const getCards = (_req: Request, res: Response, next: NextFunction) => {
   Card.find({})
     .then((cards) => res.send(cards))
-    .catch(() => res.status(DEFAULT_ERROR).send({ message: 'Ошибка сервера' }));
+    .catch(next);
 };
 
-export const createCard = (req: CustomRequest, res: Response) => {
+export const createCard = (req: CustomRequest, res: Response, next: NextFunction) => {
   const { name, link } = req.body;
   Card.create({
     name,
@@ -26,9 +26,10 @@ export const createCard = (req: CustomRequest, res: Response) => {
     .then((card) => res.send(card))
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        return res.status(INVALID_DATA_ERROR).send({ message: 'Переданы некорректные данные' });
+        next(new BadReq('Переданы некорректные данные'));
+      } else {
+        next(err);
       }
-      return res.status(DEFAULT_ERROR).send({ message: 'Ошибка сервера' });
     });
 };
 
@@ -55,7 +56,7 @@ export const deleteCard = (req: CustomRequest, res: Response, next: NextFunction
     });
 };
 
-export const setLike = (req: CustomRequest, res: Response) => {
+export const setLike = (req: CustomRequest, res: Response, next: NextFunction) => {
   Card.findByIdAndUpdate(
     req.params.cardId,
     { $addToSet: { likes: req.user && req.user._id } },
@@ -63,20 +64,21 @@ export const setLike = (req: CustomRequest, res: Response) => {
   )
     .then((cardInfo) => {
       if (!cardInfo) {
-        res.status(NOT_FOUND_ERROR).send({ message: 'Карточка по не найдена' });
+        throw new NotFound('Карточка не найдена');
       } else {
         res.send({ data: cardInfo });
       }
     })
     .catch((err) => {
       if (err.name === 'CastError') {
-        return res.status(INVALID_DATA_ERROR).send({ message: 'Переданы некорректные данные' });
+        next(new BadReq('Переданы некорректные данные'));
+      } else {
+        next(err);
       }
-      return res.status(DEFAULT_ERROR).send({ message: 'Ошибка сервера' });
     });
 };
 
-export const removeLike = (req: CustomRequest, res: Response) => {
+export const removeLike = (req: CustomRequest, res: Response, next: NextFunction) => {
   Card.findByIdAndUpdate(
     req.params.cardId,
     { $pull: { likes: req.user && req.user._id } },
@@ -84,15 +86,16 @@ export const removeLike = (req: CustomRequest, res: Response) => {
   )
     .then((cardInfo) => {
       if (!cardInfo) {
-        res.status(NOT_FOUND_ERROR).send({ message: 'Карточка по не найдена' });
+        throw new NotFound('Карточка не найдена');
       } else {
         res.send({ data: cardInfo });
       }
     })
     .catch((err) => {
       if (err.name === 'CastError') {
-        return res.status(INVALID_DATA_ERROR).send({ message: 'Запрашиваемый id некорректен' });
+        next(new BadReq('Переданы некорректные данные'));
+      } else {
+        next(err);
       }
-      return res.status(DEFAULT_ERROR).send({ message: 'Ошибка сервера' });
     });
 };
